e317add2154e1cb28c9d409b842d666282084393
salam <chaudharysm00@gmail.com>
2025-01-21 15:00:33 +0545

    register component

--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -1,6 +1,31 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import Form from "../../components/common/Form";
+import { registerFormControls } from "../../config";
+
+const initialState = {
+  userName: "",
+  email: "",
+  password: ""
+}
+
 const Register = () => {
+
+  const [formData, setFormData] = useState(initialState);
+
+  function onSubmit() {
+
+  }
+
   return (
-    <div>Register</div>
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">Create new Account</h1>
+        <p>Already have an Account? <Link to="/auth/login" className="font-medium ml-2 text-primary hover:underline">Login</Link></p>
+      </div>
+
+      <Form formControls={registerFormControls} buttonText={"SignUp"} formData={formData} setFormData={setFormData} onSubmit={onsubmit} />
+    </div>
   )
 }
 export default Register
